7f840c0c38bd5ac50adf23a906316c4c179950f7
Evening <evening@lkhagva-erdenes-iMac.local>
2023-01-10 17:21:53 +0800

    todo

--- electron/src/ Components/Todo.js
+++ electron/src/ Components/Todo.js
@@ -5,6 +5,8 @@ export function Todos() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState("");
+  const [editing, setEditing] = useState();
+  const [editingTexts, setEditingTexts] = useState({});
 
   function handleTextChange(e) {
     setText(e.target.value);
@@ -14,13 +16,20 @@ export function Todos() {
     if (text === "") {
       setError("utgaa bichne uu");
     } else {
-      const newTodo = {
-        text: text,
-        done: false,
-        id: uuidv4(),
-      };
-      const newTodos = [newTodo, ...todos];
-      setTodos(newTodos);
+      if (editing === undefined) {
+        const newTodo = {
+          text: text,
+          done: false,
+          id: uuidv4(),
+        };
+        const newTodos = [newTodo, ...todos];
+        setTodos(newTodos);
+      } else {
+        const newTodos = [...todos];
+        newTodos[editing].text = text;
+        setTodos(newTodos);
+        setEditing(undefined);
+      }
       setText("");
       setError("");
     }
@@ -49,22 +58,54 @@ export function Todos() {
     setTodos(newTodos);
   }
   // zasah arga 1
-  function editTodoWithPrompt(id) {
-    const newTodos = [...todos];
+  // function editTodoWithPrompt(id) {
+  //   const newTodos = [...todos];
 
-    const index = newTodos.findIndex((todo) => todo.id === id);
+  //   const index = newTodos.findIndex((todo) => todo.id === id);
 
-    const editedText = prompt("todo zasah", todos[index].text);
-    newTodos[index].text = editedText;
+  //   const editedText = prompt("todo zasah", todos[index].text);
+  //   newTodos[index].text = editedText;
 
-    setTodos(newTodos);
-  }
+  //   setTodos(newTodos);
+  // }
 
   // zasah arga 2
   // function editTodoWithCreateInput(index) {
   //   setEditing(index);
   //   setText(todos[index].text);
   // }
+  // zasah arga 3
+
+  function editTodoInLine(id, index) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = todos[index].text;
+    setEditingTexts(newEditingTexts);
+  }
+
+  function handleEditingText(id, e) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = e.target.value;
+    setEditingTexts(newEditingTexts);
+  }
+
+  function cancelEditing(id) {
+    const newEditingTexts = { ...editingTexts };
+    newEditingTexts[id] = undefined;
+    setEditingTexts(newEditingTexts);
+  }
+
+  function updateEditingText(index, id) {
+    const newTodos = { ...todos };
+    newTodos[index].text = editingTexts[id];
+    setTodos(newTodos);
+
+    cancelEditing(id);
+  }
+  function handleKeyUp(e) {
+    if (e.code === "Enter") {
+      addTodo();
+    }
+  }
 
   return (
     <div>
@@ -72,6 +113,7 @@ export function Todos() {
         value={text}
         style={{ borderColor: error ? "red" : "black" }}
         onChange={handleTextChange}
+        onKeyUp={handleKeyUp}
       />
       <button onClick={addTodo}>Nemeh</button>
 
@@ -85,22 +127,43 @@ export function Todos() {
               key={todo1.id}
               style={{ textDecoration: todo1.done ? "line-through" : "none" }}
             >
-              <input
-                type="checkbox"
-                onChange={(e) => handleDoneChange(todo1.id, e)}
-              />
-              {todo1.text}
-              {!todo1.done && (
+              {editingTexts[todo1.id] !== undefined ? (
                 <>
-                  {/* zasah arga 1  */}
-                  <button onClick={() => editTodoWithPrompt(todo1.id)}>
-                    Zasah
+                  <input
+                    value={editingTexts[todo1.id]}
+                    onChange={(e) => handleEditingText(todo1.id, e)}
+                  />
+                  <button onClick={() => cancelEditing(todo1.id)}>bolih</button>
+                  <button onClick={() => updateEditingText(index1, todo1.id)}>
+                    Hadgalah
                   </button>
-                  {/* zahas arga 2 */}
-                  {/* <button onClick={() => editTodoWithCreateInput(index1)}>Zasah</button> */}
+                </>
+              ) : (
+                <>
+                  <input
+                    type="checkbox"
+                    onChange={(e) => handleDoneChange(todo1.id, e)}
+                  />
+                  {todo1.text}
+                  {!todo1.done && (
+                    <>
+                      {/* zasah arga 1  */}
+                      {/* <button onClick={() => editTodoWithPrompt(todo1.id)}>
+                    Zasah
+                  </button> */}
+                      {/* zasah  arga 2 */}
+                      {/* <button onClick={() => editTodoWithCreateInput(index1)}>Zasah</button> */}
+                      {/* {zasah arga 3} */}
+                      <button onClick={() => editTodoInLine(todo1.id, index1)}>
+                        Zasah
+                      </button>
+                    </>
+                  )}
+                  <button onClick={() => handleDelete12(index1, d)}>
+                    Ustgah
+                  </button>
                 </>
               )}
-              <button onClick={() => handleDelete12(index1, d)}>Ustgah</button>
             </li>
           );
         })}
