3913118ab5f9507e5ff43af3585260566c04f55d
Evening <evening@lkhagva-erdenes-iMac.local>
2023-01-12 18:24:40 +0800

    todo ned edition

---
+++ electron/src/ Components/TodosNew.js
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+// import { TodosError } from "./TodosError";
+//  export function TodosNew ({ onSave }) {
+//     const [text, setText] = useState("");
+//     const [error, setError] = useState("");
+
+//     function handleTextChange(e) {
+//         setText(e.targer.value);
+//     }
+
+//     function handleSave
+//  }
